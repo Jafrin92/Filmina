6c1b48d2d4f90820c85dad36b931ff4f2f447fe1
Naima Nusrat Jafrin <102369761+Jafrin92@users.noreply.github.com>
2025-05-24 16:05:03 +0600

    random

--- src/GlobalComponent/header/Header.jsx
+++ src/GlobalComponent/header/Header.jsx
@@ -3,15 +3,19 @@ import GlobalContainer from "../globalContainer";
 import TopHeader from "./headerLayout/TopHeader";
 import Navigation from "./headerLayout/navigation";
 
-
 const Header = () => {
   return (
-    <header className="absolute left-0 top-0 w-full z-50">
-      <GlobalContainer>
-        <TopHeader />
-        <Navigation />
-      </GlobalContainer>
-    </header>
+    // <header className="absolute left-0 top-0 w-full z-50">
+    //   <GlobalContainer>
+    //     <TopHeader />
+    //     <Navigation />
+    //   </GlobalContainer>
+    // </header>
+    <>
+      <div className="bg-[url(./assets/work01.jpg)] h-[500px] bg-cover bg-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, quis.
+      </div>
+    </>
   );
 };
 
